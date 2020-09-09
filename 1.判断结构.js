014bcf91c922261839397e82bafcc1748a2b67ce
1565163445@qq.com <1565163445@qq.com>
2020-09-09 23:22:28 +0800

    1.判断语句.js

---
+++ 1.判断结构.js
@@ -0,0 +1,38 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-09 23:15:29
+ * @LastEditTime: 2020-09-09 23:20:51
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \小卡车的博客f:\Github_my_project\数据结构与算法\1.判断结构.js
+ */
+// if 语句
+var a = 10;
+var b = 2;
+if (a > 2) {
+  console.log(a / b);
+}
+
+// if-else 语句
+var c = 10;
+var d = 5;
+if (c > d) {
+  console.log(c / d);
+} else {
+  console.log(d)
+}
+
+// if-else if 语句
+var e = 10;
+var f = 5;
+var g = null
+if (e > 5) {
+  g = e / f
+  console.log(g)
+} else if (e === f) {
+  g = e + f
+  console.log(g)
+} else {
+  g = e * f
+  console.log(g)
+}
